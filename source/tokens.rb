0aee534e26481c28b80b4c304989a0885a13764b
Jeff Hykin <jeff.hykin@gmail.com>
2020-01-09 15:02:10 -0500

    - restructure and generate syntax

--- source/tokens.rb
+++ source/tokens.rb
@@ -1,13 +1,10 @@
-require_relative '../../textmate_tools.rb'
+require_relative '../directory'
+require_relative PathFor[:textmate_tools]
+
+# 
+# These are leftover from the Perl syntax, to give you an idea of how they're used
+# 
 tokens = [
-    { representation: 'ENV'    , areBuiltIn: true },
-    { representation: 'INC'    , areBuiltIn: true },
-    { representation: 'ARGV'   , areBuiltIn: true },
-    { representation: 'ARGVOUT', areBuiltIn: true },
-    { representation: 'SIG'    , areBuiltIn: true },
-    { representation: 'STDIN'  , areBuiltIn: true },
-    { representation: 'STDOUT' , areBuiltIn: true },
-    { representation: 'STDERR' , areBuiltIn: true },
     # arithmetic,
     { representation: "+",    areOperators: true, areArithmeticOperators: true },
     { representation: "-",    areOperators: true, areArithmeticOperators: true },
@@ -20,6 +17,7 @@ tokens = [
     { representation: "-=",   areOperators: true, areArithmeticOperators: true, areAssignmentOperators: true },
     { representation: "*=",   areOperators: true, areArithmeticOperators: true, areAssignmentOperators: true },
     { representation: "/=",   areOperators: true, areArithmeticOperators: true, areAssignmentOperators: true },
+    { representation: "**=",  areOperators: true, areArithmeticOperators: true, areAssignmentOperators: true },
     { representation: "%=",   areOperators: true, areArithmeticOperators: true, areAssignmentOperators: true },
     { representation: "--",   areOperators: true, areArithmeticOperators: true, },
     { representation: "++",   areOperators: true, areArithmeticOperators: true, },
@@ -30,11 +28,15 @@ tokens = [
     { representation: "<",    areOperators: true, areComparisonOperators: true },
     { representation: ">=",   areOperators: true, areComparisonOperators: true },
     { representation: "<=",   areOperators: true, areComparisonOperators: true },
+    { representation: "<=>",  areOperators: true, areComparisonOperators: true },
+    { representation: "=~",   areOperators: true, areComparisonOperators: true },
+    { representation: "!~",   areOperators: true, areComparisonOperators: true },
     # logical operators,
     { representation: "&&",   areOperators: true, areLogicalOperators: true },
-    { representation: "||",   areOperators: true, areLogicalOperators: true },
     { representation: "and",  areOperators: true, areLogicalOperators: true, areOperatorAliases: true },
+    { representation: "||",   areOperators: true, areLogicalOperators: true },
     { representation: "or",   areOperators: true, areLogicalOperators: true, areOperatorAliases: true },
+    { representation: "//",   areOperators: true, areLogicalOperators: true },
     # bitwise,
     { representation: "<<",   areOperators: true, areBitwiseOperators: true },
     { representation: ">>",   areOperators: true, areBitwiseOperators: true },
@@ -48,6 +50,16 @@ tokens = [
     { representation: "^=",   areOperators: true, areBitwiseOperators: true, areAssignmentOperators: true },
     # assignment,
     { representation: "=",    areOperators: true, areAssignmentOperators: true },
+    # other
+    { representation: ".",    areOperators: true },
+    { representation: ".=",   areOperators: true, areAssignmentOperators: true },
 ]
 
-@tokens = TokenHelper.new tokens
+@tokens = TokenHelper.new tokens, for_each_token: ->(each) do 
+    # isSymbol, isWordish
+    if each[:representation] =~ /[a-zA-Z0-9_]/
+        each[:isWordish] = true
+    else
+        each[:isSymbol] = true
+    end
+end
