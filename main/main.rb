e84929372b2bb116b310f72de1de7ca19ef90e6a
Jeff Hykin <jeff.hykin@gmail.com>
2022-06-11 19:22:53 -0500

    convert go-import to ruby pattern

--- main/main.rb
+++ main/main.rb
@@ -31,7 +31,7 @@ grammar = Grammar.fromTmLanguage(__dir__+"/modified.tmLanguage.json")
         :language_constants,
         :anonymous_pattern_1,
         :anonymous_pattern_2,
-        :anonymous_pattern_3,
+        :import_statement,
         :anonymous_pattern_4,
         :anonymous_pattern_5,
         :anonymous_pattern_6,
@@ -100,7 +100,133 @@ grammar = Grammar.fromTmLanguage(__dir__+"/modified.tmLanguage.json")
                 end_pattern: @end_of_line,
             )
         ]
-    
+    # 
+    # import_statement
+    # 
+        # "anonymous_pattern_3": {
+        #     "begin": "\\b(import)\\s+",
+        #     "beginCaptures": {
+        #         "1": {
+        #             "name": "keyword.import.go"
+        #         }
+        #     },
+        #     "end": "(?!\\G)",
+        #     "patterns": [
+        #         {
+        #             "include": "#imports"
+        #         }
+        #     ]
+        # },
+        grammar[:import_statement] = PatternRange.new(
+            start_pattern: Pattern.new(
+                Pattern.new(
+                    match: /\bimport/,
+                    tag_as: "keyword.import",
+                    # unit tests
+                    should_fully_match: [ "import" ],
+                    should_not_partial_match: [ "_import" ],
+                ).then(@spaces)
+            ),
+            end_pattern: lookAheadToAvoid(/\G/), # \G is the start of the start pattern... I'm not sure why/how this makes this PatternRange work -- Jeff
+            includes: [
+                :imports,
+            ]
+        )
+        grammar[:imports] = [
+            # {
+            #     "begin": "\\(",
+            #     "beginCaptures": {
+            #         "0": {
+            #             "name": "punctuation.definition.imports.begin.bracket.round.go"
+            #         }
+            #     },
+            #     "end": "\\)",
+            #     "endCaptures": {
+            #         "0": {
+            #             "name": "punctuation.definition.imports.end.bracket.round.go"
+            #         }
+            #     },
+            #     "patterns": [
+            #         {
+            #             "include": "#comments"
+            #         },
+            #         {
+            #             "include": "#imports"
+            #         }
+            #     ]
+            # }
+            PatternRange.new(
+                start_pattern: Pattern.new(
+                    match: /\(/,
+                    tag_as: "punctuation.definition.imports.begin.bracket.round",
+                ),
+                end_pattern: Pattern.new(
+                    match: /\)/,
+                    tag_as: "punctuation.definition.imports.begin.bracket.round",
+                ),
+                includes: [
+                    :comments,
+                    :imports, # recursive? this seems weird -- Jeff
+                ]
+            ),
+            
+            # presumably an alias
+            # {
+            #     "match": "((?!\\s+\")[^\\s]*)?\\s*((\")([^\"]*)(\"))",
+            #     "captures": {
+            #         "1": {
+            #             "name": "entity.alias.import.go"
+            #         },
+            #         "2": {
+            #             "name": "string.quoted.double.go"
+            #         },
+            #         "3": {
+            #             "name": "punctuation.definition.string.begin.go"
+            #         },
+            #         "4": {
+            #             "name": "entity.name.import.go"
+            #         },
+            #         "5": {
+            #             "name": "punctuation.definition.string.end.go"
+            #         }
+            #     }
+            # },
+            Pattern.new(
+                # from: ((?!\\s+\")([^\\s]*))?
+                maybe( # this maybe() doesn't make sense to me, idk whats being avoided
+                    
+                    # from: (?!\\s+\")
+                    lookAheadToAvoid(
+                        Pattern.new(@spaces).then(/"/)
+                    # from: ([^\\s]*)
+                    ).then(
+                        match: zeroOrMoreOf(/[^\s]/), # this doesnt seem like a great pattern to me, should probably be more exclusive  -- Jeff
+                        tag_as: "entity.alias.import",
+                    )
+                # from: \\s*
+                ).maybe(
+                    @spaces
+                # from: ((\")([^\"]*)(\"))
+                ).then(
+                    tag_as: "string.quoted.double",
+                    match: Pattern.new(
+                        # from: (\")
+                        Pattern.new(
+                            match: /"/,
+                            tag_as: "punctuation.definition.string.begin",
+                        # from: ([^\"]*)
+                        ).then(
+                            match: zeroOrMoreOf(/[^"]/),  # this doesnt seem like a great pattern to me, should probably be more exclusive -- Jeff
+                            tag_as: "entity.name.import",
+                        # from: (\")
+                        ).then(
+                            match: /"/,
+                            tag_as: "punctuation.definition.string.end",
+                        ),
+                    )
+                )
+            ),
+        ]
     
 #
 # Save
